234aa567d64e928cd79b2f003b6a40a8af2e1e3c
Tr-May <may.passionateteacher@gmail.com>
2023-06-13 07:17:15 +0630

    added todo list

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import InputTodo from "./InputTodo";
 import TodoList from "./TodoList";
 
 const TodosLogic = () => {
-  const todos = [
+  const [todos, setTodos] = useState([
     {
       id: 1,
       title: "Setup development environment",
@@ -18,11 +19,38 @@ const TodosLogic = () => {
       title: "Deploy to live server",
       completed: false,
     },
-  ];
+  ]);
+
+  const handleChange = (id) => {
+    setTodos((prevState) =>
+      prevState.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const delTodo = (id) => {
+    setTodos([
+      ...todos.filter((todo) => {
+        return todo.id !== id;
+      }),
+    ]);
+  };
+
   return (
     <div>
       <InputTodo />
-      <TodoList todosProps={todos} />
+      <TodoList
+        todosProps={todos}
+        handleChange={handleChange}
+        delTodo={delTodo}
+      />
     </div>
   );
 };
