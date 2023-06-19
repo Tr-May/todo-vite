d6c92784d0bf8313104cd0cf07ef491b5e687dc6
Tr-May <may.passionateteacher@gmail.com>
2023-06-19 17:15:29 +0630

    add styles for the pages

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,25 +1,35 @@
 import { useState } from "react";
 import InputTodo from "./InputTodo";
 import TodoList from "./TodoList";
+import { v4 as uuidv4 } from "uuid";
 
 const TodosLogic = () => {
   const [todos, setTodos] = useState([
     {
-      id: 1,
+      id: uuidv4(),
       title: "Setup development environment",
       completed: true,
     },
     {
-      id: 2,
+      id: uuidv4(),
       title: "Develop website and add content",
       completed: false,
     },
     {
-      id: 3,
+      id: uuidv4(),
       title: "Deploy to live server",
       completed: false,
     },
   ]);
+
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
 
   const handleChange = (id) => {
     setTodos((prevState) =>
@@ -45,7 +55,7 @@ const TodosLogic = () => {
 
   return (
     <div>
-      <InputTodo />
+      <InputTodo addTodoItem={addTodoItem} />
       <TodoList
         todosProps={todos}
         handleChange={handleChange}
